534f15a72ab96d6c070789cb018ce9f76c379e28
Marvin Ramirez <mramirez@constantevolve.ca>
2017-08-22 00:57:49 -0400

    api functionality added plus styles.2

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,13 @@
 var map;
 // Create a new blank array for all the listing markers.
 var markers = [];
-
 function initMap() {
 
-	var styles = [{
+	var styles = [
+		{
 			"featureType": "all",
-			"stylers": [{
+			"stylers": [
+				{
 					"saturation": 0
 				},
 				{
@@ -16,25 +17,32 @@ function initMap() {
 		},
 		{
 			"featureType": "road",
-			"stylers": [{
-				"saturation": -70
-			}]
+			"stylers": [
+				{
+					"saturation": -70
+				}
+			]
 		},
 		{
 			"featureType": "transit",
-			"stylers": [{
-				"visibility": "off"
-			}]
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
 		},
 		{
 			"featureType": "poi",
-			"stylers": [{
-				"visibility": "off"
-			}]
+			"stylers": [
+				{
+					"visibility": "off"
+				}
+			]
 		},
 		{
 			"featureType": "water",
-			"stylers": [{
+			"stylers": [
+				{
 					"visibility": "simplified"
 				},
 				{
@@ -45,13 +53,13 @@ function initMap() {
 	];
 	// Constructor creates a new map - only center and zoom are required.
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {
-			lat: 43.6426916,
-			lng: -79.3986131
-		},
-		zoom: 13,
-		styles: styles,
-		mapTypeControl: false
+	 center: {
+lat: 43.6426916,
+lng: -79.3986131
+},
+zoom: 13,
+styles: styles,
+mapTypeControl: false
 	});
 	// These are the real estate listings that will be shown to the user.
 	// Normally we'd have these in a database instead.
@@ -77,13 +85,13 @@ function initMap() {
 		markers.push(marker);
 
 		locations[i].marker = marker;
-
+	
 		marker.addListener('click', toggleBounce);
 		// Create an onclick event to open an infowindow at each marker.
-		marker.addListener('click', function () {
+		marker.addListener('click', function() {
 			populateInfoWindow(this, largeInfowindow);
 			console.log('clicked');
-
+			
 		});
 		bounds.extend(markers[i].position);
 	}
@@ -98,126 +106,124 @@ function populateInfoWindow(marker, infowindow) {
 	if (infowindow.marker != marker) {
 		var apiURL = 'https://api.foursquare.com/v2/venues/';
 		var foursquareClientID = '5JI5OMRGWLPI2EY5T4K44KUE4BC5AKKY4CCBEHQ1MECTT4YU';
-		var foursquareSecret = 'I0HJT34P2IUV554D1LBEQBOP2NCHETN0DXGUOTQ5EGNMAWJT';
+		var foursquareSecret ='I0HJT34P2IUV554D1LBEQBOP2NCHETN0DXGUOTQ5EGNMAWJT';
 		var foursquareVersion = '20170112';
-
-		var foursquareURL = apiURL + marker.fourSQ + '?client_id=' + foursquareClientID + '&client_secret=' + foursquareSecret + '&v=' + foursquareVersion;
-
-
+		
+		var foursquareURL = apiURL + marker.fourSQ + '?client_id=' + foursquareClientID +  '&client_secret=' + foursquareSecret +'&v=' + foursquareVersion;
+		
+		
 		$.ajax({
-			url: foursquareURL,
-			success: function (data) {
-				var desc = data.response.venue.location.formattedAddress;
-				var descURL = data.response.venue.url;
-				var fsqURL = data.response.venue.canonicalUrl;
-				infowindow.marker = marker;
-				infowindow.setContent('<h3>' + marker.title + '</h3>' + desc + '<br> <br> <a href="' + descURL + '" target="_blank">MAIN SITE</a> | <a href="' + fsqURL + '" target="_blank">FOURSQUARE</a>');
-				infowindow.open(map, marker);
-				// Make sure the marker property is cleared if the infowindow is closed.
-				infowindow.addListener('closeclick', function () {
-					infowindow.setMarker = null;
-				});
-			},
+			url: foursquareURL, 
+			success: function(data) {
+			  var desc = data.response.venue.location.formattedAddress;
+			  var descURL = data.response.venue.url;
+			  var fsqURL = data.response.venue.canonicalUrl;
+			  infowindow.marker = marker;
+			  infowindow.setContent('<h3>' + marker.title + '</h3>' + desc + '<br> <br> <a href="' + descURL + '" target="_blank">MAIN SITE</a> | <a href="' + fsqURL + '" target="_blank">FOURSQUARE</a>');
+			  infowindow.open(map, marker);
+			  // Make sure the marker property is cleared if the infowindow is closed.
+			  infowindow.addListener('closeclick',function(){
+				  infowindow.setMarker = null;
+			  });
+			}, 
 
-			error: function (data) {
+			error: function(data) {
 				// Error handler when request to Foursquare fails
 				alert("Details not currently available.");
-			}
-		});
+			  }
+		  });
 
 	}
 }
 
 
 var locations = [{
-		title: 'Art Gallery of Ontario',
-		id: '4ad4c05ef964a520daf620e3',
-		location: {
-			lat: 43.6536,
-			lng: -79.3925
-		},
-
+	title: 'Art Gallery of Ontario',
+	id: '4ad4c05ef964a520daf620e3',
+	location: {
+		lat: 43.6536,
+		lng: -79.3925
 	},
-	{
-		title: 'CN Tower',
-		id: '4ad4c05ef964a52096f620e3',
-		location: {
-			lat: 43.6426,
-			lng: -79.3871
-		}
-	},
-	{
-		title: 'The Distillery Historic District',
-		id: '4ad4c05ef964a520bff620e3',
-		location: {
-			lat: 43.6503,
-			lng: -79.3596
-		}
-	},
-	{
-		title: 'St. Lawrence Market',
-		id: '4ad4c062f964a520fbf720e3',
-		location: {
-			lat: 43.6491,
-			lng: -79.3718
-		}
-	},
-	{
-		title: 'Casa Loma',
-		id: '4bef48fcc80dc9284ec827e3',
-		location: {
-			lat: 43.6780,
-			lng: -79.4094
-		}
-	},
-	{
-		title: 'Nathan Phillips Square',
-		id: '4ad4c05ef964a520a6f620e3',
-		location: {
-			lat: 43.6525,
-			lng: -79.3835
-		}
-	},
-	{
-		title: 'Hockey Hall of Fame',
-		id: '4ad4c05ef964a520d8f620e3',
-		location: {
-			lat: 43.6473,
-			lng: -79.3777
-		}
+	
+},
+{
+	title: 'CN Tower',
+	id: '4ad4c05ef964a52096f620e3',
+	location: {
+		lat: 43.6426,
+		lng: -79.3871
 	}
+},
+{
+	title: 'The Distillery Historic District',
+	id: '4ad4c05ef964a520bff620e3',
+	location: {
+		lat: 43.6503,
+		lng: -79.3596
+	}
+},
+{
+	title: 'St. Lawrence Market',
+	id: '4ad4c062f964a520fbf720e3',
+	location: {
+		lat: 43.6491,
+		lng: -79.3718
+	}
+},
+{
+	title: 'Casa Loma',
+	id: '4bef48fcc80dc9284ec827e3',
+	location: {
+		lat: 43.6780,
+		lng: -79.4094
+	}
+},
+{
+	title: 'Nathan Phillips Square',
+	id: '4ad4c05ef964a520a6f620e3',
+	location: {
+		lat: 43.6525,
+		lng: -79.3835
+	}
+},
+{
+	title: 'Hockey Hall of Fame',
+	id: '4ad4c05ef964a520d8f620e3',
+	location: {
+		lat: 43.6473,
+		lng: -79.3777
+	}
+}
 
 ];
 
 
 function toggleBounce() {
-	var self = this;
-	self.setAnimation(google.maps.Animation.BOUNCE);
-	setTimeout(function () {
-		self.setAnimation(null);
-	}, 1450);
-}
+		var self = this;
+		self.setAnimation(google.maps.Animation.BOUNCE);
+		setTimeout(function(){self.setAnimation(null);}, 1450);
+	}
 
 var viewModel = {
-	query: ko.observable(''),
-	markerListBounce: function (location) {
+query: ko.observable(''),
+markerListBounce: function(location) {
 		google.maps.event.trigger(location.marker, 'click');
 		console.log('list cliked!!!');
-	}
+    }
 
 };
 
-viewModel.locations = ko.dependentObservable(function () {
-	var search = this.query().toLowerCase();
-	return ko.utils.arrayFilter(locations, function (location) {
-		var match = location.title.toLowerCase().indexOf(search) >= 0;
-		console.log(location);
-
-		if (location.marker) {
-			location.marker.setVisible(match);
-		}
+viewModel.locations = ko.dependentObservable(function() {
+var search = this.query().toLowerCase();
+return ko.utils.arrayFilter(locations, function(location) {    
+	var match = location.title.toLowerCase().indexOf(search) >= 0;  
+	console.log(location);
+	
+if (location.marker){
+	location.marker.setVisible(match);
+}
 		return match;
-	});
+});
 
 
 }, viewModel);
